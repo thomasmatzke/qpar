c945a0e67d5258fb30f1df7997d37fd77fc87c7d
unknown <thomasm@.(none)>
2010-02-11 15:26:12 +0100

    Literalcountheuristic added

---
+++ src/main/java/logic/LCHeuristic.java
@@ -0,0 +1,50 @@
+package main.java.logic;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+public class LCHeuristic implements Heuristic {
+
+	private Map<Integer,Integer> literalCounts;
+	private Vector<Integer> res = new Vector<Integer>();
+	
+	
+	@Override
+	public Vector<Integer> decide(Qbf qbf) {
+		literalCounts = qbf.getLiteralCount();
+		return res;
+	}
+
+	private void sortByCount(){
+		Set 		set 		= literalCounts.entrySet();
+		Map.Entry[] entries 	= new Map.Entry[set.size()];
+		Iterator 	iterator 	= set.iterator();
+		int 		count 		= 0;
+		
+		while(iterator.hasNext()) {
+			entries[count++] = (Map.Entry) iterator.next();
+		}
+		
+		Arrays.sort(entries, new Comparator() {
+			public int compareTo(Object lhs, Object rhs) {
+				Map.Entry le = (Map.Entry)lhs;
+				Map.Entry re = (Map.Entry)rhs;
+				return ((Comparable<Comparable>)le.getValue()).compareTo((Comparable)re.getValue());
+			}
+
+			@Override
+			public int compare(Object lhs, Object rhs) {
+				return compareTo(lhs, rhs);
+			}
+		});
+		
+		for(int i = entries.length-1; i >= 0; i--) {
+			res.add((Integer) entries[i].getKey());
+		}
+	}
+	
+}
