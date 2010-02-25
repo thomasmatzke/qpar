af53188873deb439f2a1312f82debb5d17d98632
thomasm <thomasm@.(none)>
2010-02-25 16:12:42 +0100

    Test stuff for georg :P

---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.io.IOException;
+
+import main.java.logic.Qbf;
+
+public class Test {
+	
+	Test() throws IOException {
+		new Qbf(null);
+	}
+		
+	public static void main() throws IOException {
+		new Test();
+	}
+}
