986fb596db44a3d84290bb112633867fa8de8019
thomasm <thomasm@.(none)>
2010-09-17 16:03:59 +0200

    Added transportlistener

---
+++ src/main/java/slave/MsgBrokerTransportListener.java
@@ -0,0 +1,39 @@
+package main.java.slave;
+
+import java.io.IOException;
+
+import main.java.slave.solver.Solver;
+
+import org.apache.activemq.transport.TransportListener;
+import org.apache.log4j.Logger;
+
+public class MsgBrokerTransportListener implements TransportListener {
+
+	static Logger logger = Logger.getLogger(SlaveDaemon.class);
+	
+	@Override
+	public void onCommand(Object arg0) {
+		// not needed
+	}
+
+	@Override
+	public void onException(IOException e) {
+		logger.error(e);
+		SlaveDaemon.master.connected = false;
+		for(Solver thread : SlaveDaemon.threads.values()) {
+			thread.kill();
+		}
+		SlaveDaemon.master.connect(SlaveDaemon.master_str);
+	}
+
+	@Override
+	public void transportInterupted() {
+		// not needed
+	}
+
+	@Override
+	public void transportResumed() {
+		// not needed
+	}
+
+}
