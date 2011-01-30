77a9854aa175c7791ba8b5043e580fd11a46a3d9
Thomas Matzke <thomas@matzke.at>
2011-01-30 16:44:28 +0100

    Better mailed exceptions

---
+++ src/main/java/StackTraceUtil.java
@@ -0,0 +1,19 @@
+package main.java;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+
+/**
+* Simple utilities to return the stack trace of an
+* exception as a String.
+*/
+public final class StackTraceUtil {
+
+  public static String getStackTrace(Throwable aThrowable) {
+    final Writer result = new StringWriter();
+    final PrintWriter printWriter = new PrintWriter(result);
+    aThrowable.printStackTrace(printWriter);
+    return result.toString();
+  }
+}
